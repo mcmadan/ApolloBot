3ed2ce0ab2242671c2bf881e5cb996ac7084df57
mcmadan <149359725+mcmadan@users.noreply.github.com>
2025-05-01 14:27:04 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
-print('Bot principal')
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.utils import executor
+import os
+
+API_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start", "help"])
+async def send_welcome(message: Message):
+    await message.reply("Inteligência ativada")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
